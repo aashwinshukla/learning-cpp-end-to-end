85c728498136506338a90f4798fcedc075162d54
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-03 15:15:48 +0530

    feat(chapter-01): Add comments example and organize chapter files
    - Add .gitignore to exclude compiled executables (*.exe)
    - Rename first-program.cpp to 1.first-program.cpp for consistent numbering
    - Add 2.comments.cpp demonstrating single-line (//) and multi-line (/**/) comment syntax
    - Establish sequential file naming convention for chapter examples

---
+++ Chapter-01-Diving-In/2.comments.cpp
@@ -0,0 +1,12 @@
+// you would have already noticed everything i wrote in // is getting ignored by the program
+// the reason is everything after // is called comment which cant be read by program and can be used to write instruction or 
+// functionality of a program 
+
+// another way to comment longer text or even code is as follow using /**/
+
+/*
+    #include <iostream>
+    int main(){
+        std::<<"Hello"<<std::endl;
+    }
+*/
