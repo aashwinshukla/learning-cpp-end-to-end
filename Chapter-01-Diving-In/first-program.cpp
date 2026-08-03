83a66a231ed22a488c7667c3bb62faf40e487a1a
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-03 15:07:11 +0530

    feat(chapter-01): Implement hello world program with I/O examples
    - Add complete first C++ program template with main function
    - Include iostream library for standard input/output operations
    - Demonstrate std::cout with "hello world!" output
    - Add std::endl usage examples showing line break behavior
    - Include compilation instructions using g++ compiler
    - Provide inline comments explaining each code section and concepts
    - Show practical examples of printing multiple values with and without newlines

---
+++ Chapter-01-Diving-In/first-program.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+// we first include libraries that will halp us write the code and help program to understand specific keywords (function/commands)
+// which we will learn as we go.
+
+int main(){
+    // this is the main function where, you can say our body of the code resides.
+    std::cout << "hello world!" << std::endl;
+    // the above line of code prints out hello world! on the terminal.
+    // to compile the code write g++ first-program.cpp -o first-program 
+    // to run it , use the compiled .exe file ie, .\first-program.exe
+
+    // the std::endl creates i new line character in other worlds if we write 
+    std::cout <<"Number 1";
+    std::cout << "Number 2";
+    // they will be printed on the same line
+    // to avoid that add <<std::endl
+    // and now number 2 will be on new line
+    
+    // std::cout"" helps in printing things out 
+    // we are able to use it because of <isotream> library
+    
+    return 0;
+
+}
