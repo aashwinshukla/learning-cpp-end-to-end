13beb43dd38a8be8241e524cc1c482362aa06138
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-05 17:38:10 +0530

    feat(chapter-04): Add ternary operator example and documentation
    - Add ternary operator syntax explanation with format documentation
    - Include comparative example showing if-else statement vs ternary operator
    - Demonstrate practical use case of finding maximum value using ternary operator
    - Provide executable code examples for both traditional and ternary approaches
    - Help readers understand concise conditional expression syntax

---
+++ Chapter-04-Flow-Control/5.ternary-opt.cpp
@@ -0,0 +1,25 @@
+// the way of writing this formate is 
+
+result = (condition) ? option 1 : option 2;
+
+// so example is 
+// in if else statemnet 
+
+int max {0};
+
+int a {10};
+int b {12};
+
+if(a>b){
+    max = a;
+}else{
+    max = b;
+}
+
+std::cout<< "max: "<< max << std::endl;
+
+// in ternary 
+
+int max = (a > b)? a : b ;  
+std::cout<< "max: "<< max << std::endl;
+
