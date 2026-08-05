16a92ad8c22cf38ecc6bd538e8f8ac3263b239f6
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-05 15:56:48 +0530

    feat(chapter-03): Add output formatting documentation
    - Document std::flush for flushing output to terminal
    - Document std::setw() for setting output width and alignment
    - Document std::setfill() for filling spaces with custom characters
    - Document std::showpos for displaying + symbol with positive numbers
    - Document number system formatting with std::dec, std::oct, std::hex
    - Document std::uppercase for uppercase output conversion
    - Document std::fixed and std::scientific for numeric representation
    - Document std::setprecision for controlling decimal precision
    - Document std::showpoint for showing decimal point on all numbers
    - Provides reference comments for common output formatting techniques

---
+++ Chapter-03-Operations-on-Data/8.output-formating.cpp
@@ -0,0 +1,21 @@
+#include<ios>
+#include<iomanip>
+//will be used for formating 
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // std::flush = this helps to put all output to terminal at once 
+    // std::setw{} = this will set width and make things look more readable 
+    // in setw{} we can also use std::right or std::left to place the data
+    // std::setfill = this fill a character of your choice in the space between the data 
+    // std::showpos = this will show + symbol infront of positive number.
+    // std::dec, std::oct, std::hex = these three will show the datain their respective number system.
+    // std::uppercase = this will put everything in cap
+    // std::fixed, std::scientific = better representation
+    // std::setprecision = set decimal precision (but also depends on data type capacity)
+    // std::showpoint = this show decimal for every type of number
+     
+    return 0;
+}
