e69b5a97bdbce2bfe83450e22e303a571ec3d880
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-05 14:38:33 +0530

    feat(chapter-03): Add operator precedence and associativity documentation
    - Add explanation of operator precedence concept and its role in expression evaluation
    - Add explanation of associativity and its direction of operation solving
    - Document that multiplication and division have higher precedence than addition and subtraction
    - Include note on left-to-right evaluation order for operators of equal precedence
    - Explain how C++ follows standard operator precedence rules

---
+++ Chapter-03-Operations-on-Data/3.precedence-associativity.cpp
@@ -0,0 +1,12 @@
+// if we want to calculte the bigger problem that has multiple operation like
+
+// a + b * c - d/e - f + g
+
+// in above equation we might want to solve different sections in different sequence/order
+// this is wheree precedance and associativity
+
+// precedance : which opt to do first 
+// associativity : which direction to start solving 
+
+//so all we have to do /remember is dicision and multiplication comes first and then left to right order.
+// C== follows these rules and you can find more online about this .
