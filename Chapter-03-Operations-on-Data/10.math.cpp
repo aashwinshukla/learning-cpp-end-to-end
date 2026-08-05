b7a4db109dc06945e7380e5c830844faa3cf035e
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-05 16:29:35 +0530

    feat(chapter-03): Add math functions documentation and reference guide
    - Document floor() function for rounding down to lowest integer
    - Document ceil() function for rounding up to highest integer
    - Document abs() function for absolute value calculation
    - Document trigonometric functions (cos, sin, tan)
    - Document exp() function for exponential calculations
    - Document log() function for logarithmic calculations
    - Document pow() function for power operations with base and exponent
    - Document sqrt() function for square root calculations
    - Document round() function for standard rounding operations
    - Provide quick reference comments for common math operations from cmath library

---
+++ Chapter-03-Operations-on-Data/10.math.cpp
@@ -0,0 +1,15 @@
+// maths functions 
+//use 
+
+#include <cmath>
+using namespace std;
+
+// floor() = round to lowest like 2.9 to 2
+// ceil() = round to highest like 2.1 to 3
+// abs() = give absolute value 
+// cos(), sin(), tan()
+// exp() = anything inside the bracket will be e raise to pwer that number 
+// log()
+// pow() = pow can be used like pow(a, b) where a is base b is power
+// sqrt() = square root of the number 
+// round()
