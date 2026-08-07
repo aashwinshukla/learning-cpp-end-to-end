6e2e57f28e64e6f0372941d9a3251df24481d2cf
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-07 16:26:07 +0530

    feat(chapter-06): Add arrays introduction and declaration examples
    - Add array concept explanation as a collection of data with single identity
    - Add declaring-and-using example demonstrating array declaration syntax
    - Include for loop example showing how to iterate through array elements
    - Add explanatory comments about garbage values in uninitialized arrays
    - Establish foundation for arrays chapter following loops chapter progression

---
+++ Chapter-06-Arrays/1.intro.cpp
@@ -0,0 +1,3 @@
+// array = collection of data and give them a single identity
+
+
---
+++ Chapter-06-Arrays/2.declaring-and-using.cpp
@@ -0,0 +1,12 @@
+// lets see how to declare and use them.
+using namespace std;
+
+int score [10];  // array storing 10 integers.
+
+for(size_t i{0}; i<10; i++){
+    cout<< "Score " << i+1 << ": "<< score[i]<<endl;
+}
+
+// now this will print out all the 10 value of score stored inside the array score[]
+// thing to notice since we didnt added anything all value will be garbage value 
+
