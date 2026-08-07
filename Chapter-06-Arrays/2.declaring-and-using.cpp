0067e59aaaed1e1430cd72b6893dde060a1fb308
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-07 16:31:27 +0530

    feat(chapter-06): Add array initialization and indexing explanation
    - Update comment to clarify array declaration purpose
    - Add explanation of zero-based array indexing
    - Include practical example of initializing array values using a loop
    - Add guidance on assigning values to avoid garbage values in arrays
    - Document best practice for populating array elements before use

--- Chapter-06-Arrays/2.declaring-and-using.cpp
+++ Chapter-06-Arrays/2.declaring-and-using.cpp
@@ -1,4 +1,4 @@
-// lets see how to declare and use them.
+// lets see how to declare and use array.
 using namespace std;
 
 int score [10];  // array storing 10 integers.
@@ -9,4 +9,16 @@ for(size_t i{0}; i<10; i++){
 
 // now this will print out all the 10 value of score stored inside the array score[]
 // thing to notice since we didnt added anything all value will be garbage value 
+
+//btw indexing starts from 0.
+// so when we say score[10] array that means array has value at memory 0 to 9 which we are acessing using i and for loop
+
+for(size_t i{0}; i<10; ++i){
+    score[i] = i * 5;
+}
+
+// I wrote above code so you can add this before printing out the values in array,
+// so you all dont get garbage value 
+// and above is one of the best way to assign value to memory inside the array
+
 
