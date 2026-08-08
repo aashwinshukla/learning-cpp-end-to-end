8af3bebbea95c7e9eb5fe4bb56d9bd421f5e5f1e
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-08 17:42:54 +0530

    feat(chapter-10): Add one definition rule example and documentation
    - Create new file explaining the one definition rule (ODR) in C++
    - Document that definitions cannot appear more than once in a translation unit
    - Explain the error conditions when duplicating variables or functions
    - Note the exception for class definitions across different translation units
    - Add clarification on struct redefinition rules and compilation behavior

---
+++ Chapter-10-Functions/1.one-defination-rule.cpp
@@ -0,0 +1,9 @@
+//defination cants show up more than once in entire program, or translation unit.
+
+// if we define a single variable or function more than once in the same file or outside of the file (location doesnt matter)
+// it is considered error and need to be fixed
+
+// the only exceptions are classes 
+// you can hace struct in two place and code will still compile and run.
+// the only condition is defination of same classes  should be in different teranslation unit.
+
