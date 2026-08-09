9aa91612cbf4d2da80f68048697536a6dd6f412a
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-09 15:00:33 +0530

    feat(chapter-10): Add multiple files inclusion example and documentation
    - Add example demonstrating how to include multiple header files in a single translation unit
    - Include syntax for including both system libraries and custom header files
    - Document the use case for organizing functions and operations across separate files
    - Provide clear comments explaining the purpose and benefits of multi-file program structure

---
+++ Chapter-10-Functions/4.multiple-files.cpp
@@ -0,0 +1,18 @@
+// when we have multiple files in the programwhich inculudes different source and function and you want to use them in one single translation unit
+
+// lets say file one is called compare.h
+// and the second file is called operations.h
+
+// to use them write the syntax as below 
+
+#include <iostream>
+#include <strings>
+......
+......//all the required libraries
+
+#include "compare.h"
+#include "operations.h"
+
+// this way you will be able to use their context in your 
+// current translation unit
+
