4fbb0df47149cc76f96ed70110427e78e9f0f1f3
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-09 15:17:01 +0530

    feat(chapter-10): Add pass-by-value parameter passing example
    - Create pass-by-value example demonstrating function parameter behavior
    - Show how modifications to function parameters don't affect original variables
    - Include age variable demonstration with increment operation
    - Add comments explaining that changes to parameter copy don't modify original value
    - Document the distinction between pass-by-value and other parameter passing methods

---
+++ Chapter-10-Functions/5.pass-by-value.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+
+void say_age(int age);
+
+int main(){
+    int age {23};
+
+    cout<< "age before" << age << endl;
+    say_age(age);
+    cout<< "age after" << age << endl;
+
+    return 0;
+    
+}
+
+void say_age(int age){
+    ++age;
+    cout << "hello! you are "<< age << " years old "<< endl;
+// changing copy of age here will not change original age that is in main 
+}
+
