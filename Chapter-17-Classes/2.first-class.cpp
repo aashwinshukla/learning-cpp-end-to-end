aec1fd7bb0534bf6a5a140e57013c07aad24df68
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-11 19:08:48 +0530

    feat(chapter-17): Expand first class example with includes and member documentation
    - Add iostream header and using namespace std for I/O capabilities
    - Add PI constant definition for future volume calculations
    - Clarify member variables with inline comment
    - Add explanatory comments about member variable constraints and access patterns
    - Document that class methods have access to private member variables

--- Chapter-17-Classes/2.first-class.cpp
+++ Chapter-17-Classes/2.first-class.cpp
@@ -3,11 +3,15 @@
 // lets make our own data type, for our own uses
 
 // Syntax
+#include <iostream>
+using namespace std; 
+
+const double PI {3.14};
 
 class Cylinder{
     public :
         double base_radius {1.0};   // these are going to define properties of the class
-        double height {1.0};
+        double height {1.0};        // member variables
 
     public :                        // next part makes up the behaviour of the class
         double volume(){            // it uses properties to work as function for the class
@@ -37,3 +41,9 @@ int main(){
 // all parts of the class are private by default 
 // so have to add the keyword public to acesses functions like volume :
 // cylinder1.volume()
+
+
+// members variable cant be references
+// class methods have acesses to the member variable, 
+// even if they are private
+ 
