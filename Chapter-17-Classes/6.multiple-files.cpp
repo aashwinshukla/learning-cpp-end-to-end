03ccf26ca739f19ddd6465158c218d7ae5a4c58d
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-11 20:03:57 +0530

    feat(chapter-17): Add multiple-files example with header guards and constants
    - Fix include statements to use .h file extensions for constants and cylinder headers
    - Add header guard pattern (#ifndef, #define, #endif) to prevent multiple inclusions
    - Add PI constant definition as example for constants.h file
    - Demonstrate best practices for organizing C++ code across multiple files

--- Chapter-17-Classes/6.multiple-files.cpp
+++ Chapter-17-Classes/6.multiple-files.cpp
@@ -4,5 +4,15 @@
 // make file called constants.h and fill it will const
 // make file called cylinder.h and fill it with class Cylinder
 
-#include "constants"
-#include "cylinder"
+#include "constants.h"
+#include "cylinder.h"
+
+
+// you can also in the constant.h file 
+
+#ifndef CYLINDER_H
+#define CYLINDER_H
+
+const double PI {3.141592};
+
+#endif
