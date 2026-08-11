e160573f1ec55940b4f3ef6f528ab5dc0d1f86d5
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-11 19:01:58 +0530

    feat(chapter-17): Add classes introduction and first class example
    - Add intro file with foundational class concepts explanation
    - Add first class example demonstrating Cylinder class creation
    - Illustrate class properties (base_radius, height) and methods (volume)
    - Demonstrate object instantiation and member access patterns
    - Include comments explaining public access modifiers and default privacy rules
    - Provide practical usage examples with multiple object instances

---
+++ Chapter-17-Classes/1.intro.cpp
@@ -0,0 +1,3 @@
+// we learn about classes and how they are blue print to the object we will make inside them, 
+
+// add proper intro 
---
+++ Chapter-17-Classes/2.first-class.cpp
@@ -0,0 +1,39 @@
+// your first C++ class
+
+// lets make our own data type, for our own uses
+
+// Syntax
+
+class Cylinder{
+    public :
+        double base_radius {1.0};   // these are going to define properties of the class
+        double height {1.0};
+
+    public :                        // next part makes up the behaviour of the class
+        double volume(){            // it uses properties to work as function for the class
+            return PI * base_radius * height;
+        }    
+
+};
+
+// now we can use the Cylinder type we created ourself
+
+int main(){
+    Cylinder cylinder1;
+    cout << "Vloume c1 : " << cylinder1.volume() << endl;
+
+    cylinder1.base_radius = 3.0;
+    cylinder1.height = 2;
+    cout << "Vloume c1 : " << cylinder1.volume() << endl;
+
+    Cylinder cylinder2;
+    cout << "Volume c2 : "<< cylinder2.volume() << endl;
+
+    return 0;
+}
+
+
+// VERY IMP:
+// all parts of the class are private by default 
+// so have to add the keyword public to acesses functions like volume :
+// cylinder1.volume()
