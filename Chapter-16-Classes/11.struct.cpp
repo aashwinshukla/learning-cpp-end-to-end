8f906d8b112934a67e4221deeea62c22be3c596b
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-12 13:02:48 +0530

    feat(chapter-16): Add struct and class member access difference example
    - Add Dog class example demonstrating private member access by default
    - Add Cat struct example demonstrating public member access by default
    - Include documentation on using private keyword in structs for consistency
    - Clarify the distinction between class and struct default access modifiers

---
+++ Chapter-16-Classes/11.struct.cpp
@@ -0,0 +1,15 @@
+// struct and class difference 
+
+
+// members are private by default
+class Dog{
+    std::string_name{"none"};
+};
+
+// members are public by default 
+struct Cat{
+    std::string name;
+};
+// we can make it private by useing private: like we were doing 
+// to make public in class
+
