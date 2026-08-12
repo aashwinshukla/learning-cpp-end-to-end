4706867725f54f5017e1186571462a8cc55d1a1a
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-12 13:24:04 +0530

    feat(chapter-17): Add inheritance introduction with class hierarchy examples
    - Add Person base class definition
    - Add Engineer class inheriting from Person with public inheritance
    - Add CivilEng class inheriting from Engineer demonstrating multi-level hierarchy
    - Include comments explaining class property inheritance pattern
    - Include analogy comparing class inheritance to genetic inheritance
    - Establish foundational concepts for inheritance chapter examples

---
+++ Chapter-17-Inheritance/1.intro.cpp
@@ -0,0 +1,12 @@
+// we can use hierchy of classes and make different connection between them 
+// make this defination better
+
+class Person{}
+// then we can also make somethingrelated to  aperson
+class Engineer:public Person
+// and so on
+class CivilEng:public Engineer
+// each class has property of the previous 
+
+// justlike parents genes inherited by child 
+
