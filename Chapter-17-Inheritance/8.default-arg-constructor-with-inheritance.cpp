3c10c0f3e1f65002a0b8a0c1935d050bc2a4e467
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-12 18:10:20 +0530

    feat(chapter-17): Add default argument constructor with inheritance example
    - Add example demonstrating how default constructors are called in inheritance hierarchy
    - Show constructor call sequence from base class (Person) through intermediate class (Engineer) to derived class (CivilEng)
    - Include practical demonstration with CivilEng object instantiation
    - Document best practice of providing default constructors for classes in inheritance hierarchies
    - Help clarify constructor invocation order when inheriting from multiple levels

---
+++ Chapter-17-Inheritance/8.default-arg-constructor-with-inheritance.cpp
@@ -0,0 +1,23 @@
+// now when we inherit from one class to another how does at the last class does srg constructor are called???
+
+// lets get back to our example to person, engineer and civileng 
+// at CivilEng we will see...
+
+// person default arg constructor called....
+// Engineer default arg constructor called....
+// CivilEng default arg constructor called....
+
+// this what occurs when..
+
+int main(){
+    CivilEng civil_eng1;
+
+    return 0;
+}
+
+// is programmed 
+
+
+// always provide a default constructor for your classes
+// especially if they will be part of an inheritance hierachy
+
