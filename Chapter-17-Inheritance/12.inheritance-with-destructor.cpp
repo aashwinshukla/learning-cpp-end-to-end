663575d61054290cffbc070670ecbad7bd1aa664
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-13 15:51:24 +0530

    feat(chapter-17): Add inheritance with destructor documentation and examples
    - Add documentation explaining destructor call order in inheritance hierarchy
    - Document that destructors are called in reverse order of constructors (child to parent)
    - Include example showing Person -> Engineer -> CivilEng constructor chain
    - Document corresponding destructor call sequence (CivilEng -> Engineer -> Person)
    - Provide foundational notes for destructor behavior in inheritance context

---
+++ Chapter-17-Inheritance/12.inheritance-with-destructor.cpp
@@ -0,0 +1,8 @@
+// Destructor are called in the reverse order than the constructors
+
+// as we know...
+// Person -> Engineer -> CivilEng   is how we made the constructor 
+// CivilEng -> Engineer -> Person   is how the destructor will be called
+
+add anything that might be imp in the topic 
+
