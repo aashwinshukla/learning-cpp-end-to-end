453c6b2aa8067c5c2eff2248cda4e2c3ff5b3730
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-16 15:06:32 +0530

    feat(chapter-19): Add vector container examples with operations and iteration
    - Add vector declaration and initialization examples
    - Add push_back and emplace_back operations with pair examples
    - Add vector constructor patterns (size, fill value, copy constructor)
    - Add iterator usage including begin, end, rbegin, rend operations
    - Add iterator arithmetic and traversal examples
    - Add vector access methods (back element retrieval)
    - Add three iteration patterns (iterator loop, auto iterator, range-based for)
    - Add erase operations for single and range element deletion
    - Demonstrates core vector functionality as part of STL learning progression

---
+++ Chapter-19-STL/03.Vector.cpp
@@ -0,0 +1,70 @@
+// the first container which will learn is vector
+
+vector<int> v;
+// vector declaration
+
+v.push_back(1);
+v.emplace_back(2);
+// explain what these do 
+
+vector<pair<int, int>> vec;
+
+v.push_back{1, 2};
+v.emplace_back(1, 2);
+// explain this as well
+
+vector<int> v(5,100);  // this means {100, 100, 100, 100, 100} , ie 100 is 5 times
+vector<int>(5);  // this 5 instences of either zero or garbage value 
+
+vector<int> v1(5, 20);
+vector<int> v2(v1);
+// v2 will have similar collection of 5 20's as v1
+
+
+vector<int>::iterator it = v.begin();
+it++;
+cout<< *(it) << " ";
+// if collection is {20, 34, 56, 67}
+// then we will get 34 since we did it++ then print
+
+it = it+2;
+cout << *(it) <<" ";
+// here assuming we did it++ and then it = it + 2
+// we will get 67
+
+vector<int>::iterator it = v.end();
+// end points to memory after the last element or the place after the last element
+vector<int>::iterator it = v.rend();
+// reverse end
+vector<int>::iterator it = v.rbegin();
+// reverse begin
+
+cout << v.back() << " ";
+// last index element 
+
+// to print the vector 
+// method 1: 
+for (vector<int>::iterator it = v.begin(); it != v.end(); it++){
+
+}
+
+// method 2: 
+for (auto it = v.begin(); it != v.end(); it++ ){
+
+}
+
+// method 3: 
+for (auto it : v){
+
+}
+
+
+//now to delete an element we use erase
+
+v.erase(v.begin()+1);
+// this will remove any elemnet on +1 of the element index 0
+v.erase(v.begin()+2, v.begin()+4)
+// now this tells the range of removal of element 
+// where begin()+2 is starting point and is included 
+// whereas begin()+4 is end point which is not included and will not get deleted 
+
